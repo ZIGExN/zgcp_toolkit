106767ae5cf84e043c675742d4ccd828264d4019
hungnt0212 <70182230+hungnt0212@users.noreply.github.com>
2021-05-31 23:53:59 +0700

    Add format for request env (#5)

--- lib/zgcp_toolkit/logger.rb
+++ lib/zgcp_toolkit/logger.rb
@@ -31,7 +31,7 @@ module ZgcpToolkit
     end
 
     [:debug, :info, :warn, :error, :fatal, :unknown].each do |log_level_method|
-      define_method(log_level_method) do |log, push_slack: false|        
+      define_method(log_level_method) do |log, push_slack: false|
         log_object =
           case log
           when StandardError
@@ -51,8 +51,54 @@ module ZgcpToolkit
       end
     end
 
+    def error_request(error, request, **args)
+      filter_request_params = format_request_env(request)
+
+      error({ message: error.message, backtrace: error.backtrace.first(backtrace_limit) }.merge!(filter_request_params).merge!(args))
+    end
+
     def flush!
       loggers.each { |a| a.flush! }
+    end
+
+    private
+
+    def format_request_env(request)
+      log_object = {}
+      log_object[:request]     = request_filter(request)
+      log_object[:session]     = session_filter(request)
+      log_object[:environment] = environment_filter(request)
+      log_object
+    end
+
+    def environment_filter(request)
+      result = {}
+      request.filtered_env.keys.each do |key|
+        result[key] = request.filtered_env[key]
+      end
+      result
+    end
+
+    def session_filter(request)
+      result = {}
+      result[:session_id]   = request.ssl? ? "[FILTERED]" : request.session['session_id'] || request.env['rack.session.options'][:id].inspect
+      result[:data_session] = request.session.to_hash
+      result
+    end
+
+    def request_filter(request)
+      result = {}
+      result[:url]            = request.url
+      result[:request_method] = request.request_method
+      result[:ip_address]     = request.remote_ip
+      result[:parameters]     = request.filtered_parameters.inspect
+      result[:timestamp]      = Time.current
+      result[:server]         = Socket.gethostname
+      result[:process]        = $$
+      if defined?(Rails) && Rails.respond_to?(:root)
+        result[:rails_root] = Rails.root
+      end
+      result
     end
   end
 end
