dee73572c104ba6daafa0732d9016b891fb0c2bc
Ngo Trung Hung <hungnt@zigexn.vn>
2021-05-31 17:06:01 +0700

    add format for request env

--- lib/zgcp_toolkit/logger.rb
+++ lib/zgcp_toolkit/logger.rb
@@ -31,7 +31,7 @@ module ZgcpToolkit
     end
 
     [:debug, :info, :warn, :error, :fatal, :unknown].each do |log_level_method|
-      define_method(log_level_method) do |log, push_slack: false|        
+      define_method(log_level_method) do |log, push_slack: false|
         log_object =
           case log
           when StandardError
@@ -49,6 +49,52 @@ module ZgcpToolkit
           end
         loggers.each { |a| a.send(log_level_method, log_object) }
       end
+    end
+
+    def error_request(error, **args)
+      env = args.fetch(:env)
+      args.delete(:env)
+      formatted_env = format_env(env)
+
+      error({message: error.message, backtrace: error.backtrace.first(backtrace_limit)}.merge!(formatted_env))
+    end
+
+    def format_env(request)
+      log_object = {}
+      log_object[:request]     = request_filter(request)
+      log_object[:session]     = session_filter(request)
+      log_object[:environment] = environment_filter(request)
+      log_object
+    end
+
+    def environment_filter(request)
+      result = {}
+      request.filtered_env.keys.each do |key|
+        result[key] = request.filtered_env[key]
+      end
+      result
+    end
+
+    def session_filter(request)
+      result = {}
+      result[:session_id]   = request.ssl? ? "[FILTERED]" : request.session['session_id'] || request.env['rack.session.options'][:id].inspect
+      result[:data_session] = request.session.to_hash
+      result
+    end
+
+    def request_filter(request)
+      result = {}
+      result[:url]            = request.url
+      result[:request_method] = request.request_method
+      result[:ip_address]     = request.remote_ip
+      result[:parameters]     = request.filtered_parameters.inspect
+      result[:timestamp]      = @timestamp
+      result[:server]         = Socket.gethostname
+      result[:process]        = $$
+      if defined?(Rails) && Rails.respond_to?(:root)
+        result[:rails_root] = Rails.root
+      end
+      result
     end
 
     def flush!
